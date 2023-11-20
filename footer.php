0e9f79acbc328b4e14158ce3c484a3f254061ff8
Rumaisa Rather <144339987+rumaisarather@users.noreply.github.com>
2023-11-21 00:35:43 +0530

    Update footer.php
    Kk

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
       <div class="box">
          <a href="tel:1234567890"><i class="fas fa-phone"></i> +91-456-7890</a>
          <a href="tel:1112223333"><i class="fas fa-phone"></i> +91-222-3333</a>
-         <a href="mailto:anasbhai@gmail.com"><i class="fas fa-envelope"></i> rumaisa1rather@gmail.com</a>
+         <a href="mailto: ABC@gmail.com"><i class="fas fa-envelope"></i> XYZ@gmail.com</a>
          <a href="#"><i class="fas fa-map-marker-alt"></i> Kashmir, India - 190001</a>
       </div>
 
