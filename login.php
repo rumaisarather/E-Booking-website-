3bbd311b97f68a25323dfba998a26940f2988715
Rumaisa <144339987+rumaisarather@users.noreply.github.com>
2023-11-21 16:01:21 +0530

    Update login.php
    K

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 
-include '../components/connect.php';
+include 'connect.php';
 
 if(isset($_POST['submit'])){
 
@@ -36,7 +36,7 @@ if(isset($_POST['submit'])){
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="../css/admin_style.css">
+   <link rel="stylesheet" href="admin_style.css">
 
 </head>
 <body>
