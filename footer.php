baed8829338aab119aa675466d3f2e8a2ce6d349
Rumaisa <144339987+rumaisarather@users.noreply.github.com>
2023-12-15 17:37:15 +0530

    add

---
+++ footer.php
@@ -0,0 +1,38 @@
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +91-456-7890</a>
+         <a href="tel:1112223333"><i class="fas fa-phone"></i> +91-222-3333</a>
+         <a href="mailto:anasbhai@gmail.com"><i class="fas fa-envelope"></i> rumaisa1rather@gmail.com</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i> Kashmir, India - 190001</a>
+      </div>
+
+      <div class="box">
+         <a href="#home">home</a>
+         <a href="#about">about</a>
+         <a href="bookings.php">my bookings</a>
+         <a href="#reservation">reservation</a>
+         <a href="#gallery">gallery</a>
+         <a href="#contact">contact</a>
+         <a href="#reviews">reviews</a>
+      </div>
+
+      <div class="box">
+         <a href="#">facebook <i class="fab fa-facebook-f"></i></a>
+         <a href="#">twitter <i class="fab fa-twitter"></i></a>
+         <a href="#">instagram <i class="fab fa-instagram"></i></a>
+         <a href="#">linkedin <i class="fab fa-linkedin"></i></a>
+         <a href="#">youtube <i class="fab fa-youtube"></i></a>
+      </div>
+
+   </div>
+
+   <div class="credit">&copy; copyright @ 2023 by R.Rather Web Designer | all rights reseved!</div>
+
+</section>
+
+<!-- footer section ends -->
